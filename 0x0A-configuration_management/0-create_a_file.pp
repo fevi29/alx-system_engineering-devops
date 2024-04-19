3ca50bec479305a2a28c556eac9a622f5388dff5
feven <fevenbayu5@gmail.com>
2024-04-19 15:19:05 -0700

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#The code will create a file name school inside the /tmp Directory
+# Creates a file in /tmp
+
 file { '/tmp/school':
- ensure  => file,
+  path    => '/tmp/school', 
+ content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
