1e193f0eaaeeb18710eb0e045535321f4bcf06a9
feven <fevenbayu5@gmail.com>
2024-04-19 13:04:24 -0700

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installs puppet-lint, version 2.1.0
+# Installs puppet-lint, version 2.1.1
 
 exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.1',
 }
