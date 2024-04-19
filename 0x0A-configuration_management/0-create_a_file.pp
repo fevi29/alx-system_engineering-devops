676be012222f5e31daf4746e07613f1c8977d653
feven <fevenbayu5@gmail.com>
2024-04-19 13:42:10 -0700

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# creates a file in /tmp
-
+# A puppet manifest that creates a file school in /tmp.
+# The file (School) has permissions 0744, belonging to the user and group
+# www-data.
 file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/school',
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
